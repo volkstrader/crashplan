f08ac5f612a874ca1a3374ecd1153f5768a9daae
Chuck Chau <cchau2+github@gmial.com>
2015-05-01 16:18:01 -0700

    add latest

--- latest/start.sh
+++ latest/start.sh
@@ -3,4 +3,4 @@ docker run -d -h peenemunde --name=crashplan \
 -v /media/crashplan/archive:/media/archive \
 -v /media/crashplan/backup:/media/backup \
  -p 4222:22 -p 4242:4242 -p 4243:4243 \
-hcsoftech/crashplan:v3.7.0
+hcsoftech/crashplan:latest
